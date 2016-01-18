c3ff5b12bf9dbe134501e8f7e42ccc708d362370
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2016-01-18 14:46:10 -0500

    Group like functions in array.h

--- src/main/c/array.h
+++ src/main/c/array.h
@@ -12,29 +12,22 @@ typedef struct {
 } array;
 
 int array_create(array ** A, uint32_t length);
-
 int array_destroy(array ** A);
 
 void array_print(array * A);
 
 void array_swap(uint32_t * data, int a, int b);
-
 void array_set(array ** A, uint32_t val, uint32_t size);
-
 void array_copy(uint32_t * data_A, uint32_t * data_B, int start_A, int start_B, int num);
-
 void array_randomize(array ** A, uint32_t size);
 
-void array_sort_insertion(array * A);
-
-int array_sort_mergesort(array * A, array * B, array * C, int l, int r);
-
 int array_sort_merge(array * A, array * B, array * C, int l, int r, int mid);
-
 int array_sort_quicksort_partition(array *A, int l, int r);
 
-int array_sort_quicksort(array * A, int l, int r);
-
 int array_test_sorted(array * A);
+
+void array_sort_insertion(array * A);
+int array_sort_mergesort(array * A, array * B, array * C, int l, int r);
+int array_sort_quicksort(array * A, int l, int r);
 
 #endif
