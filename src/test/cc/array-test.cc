f246ab0cc74b122f3706694a8f8813c53a87fce3
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2016-02-07 14:37:49 -0500

    Cleanup array-test.c in preparation for more tests

--- src/test/cc/array-test.cc
+++ src/test/cc/array-test.cc
@@ -4,15 +4,18 @@
 
 bool TestInsertionSort() {
   algorithms::Array<int> a(10000);
-
   a.Randomize(0, 100);
   a.SortInsertion();
   return a.IsSorted();
 }
 
 int main() {
-  std::cout << (TestInsertionSort() ? "[PASS]" : "[FAIL]") <<
+  bool passed_all_tests = true;
+
+  bool passed_insertion_sort = TestInsertionSort();
+  passed_all_tests &= passed_insertion_sort;
+  std::cout << (passed_insertion_sort ? "[PASS]" : "[FAIL]") <<
     " InsertionSort" << std::endl;
 
-  return 0;
+  return !passed_all_tests;
 }
